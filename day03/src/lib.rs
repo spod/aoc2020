e604d1322ebdf72ad5aaaa1d889ed32a38c135a2
Michael Collins <michael.collins@gmail.com>
2020-12-02 22:27:00 -0800

    Broken day 3 part 1 - checkpoint before ripping apart :(

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -17,10 +17,45 @@ impl Problem for Day03 {
     }
 }
 
-fn part1(_: &str) -> () {
-    ()
+fn part1(input: Vec<&str>) -> i8 {
+    let grid = parse_input(input);
+    let x_move = 3;
+    let y_move = 1;
+    let wrap = grid[0].len();
+
+    let mut count = 0;
+    let mut x = 0;
+    let mut y = 0;
+    while y < grid.len() - 1 {
+        println!("x,y at start: {},{}", x, y);
+        x = (x + x_move) % wrap;
+        y = y + y_move;
+        println!("x,y after step: {},{}", x, y);
+        println!("grid[x][y]: {}", grid[x][y]);
+        if grid[x][y] == 1 {
+            println!("Ow!");
+        }
+        count = count + grid[x][y];
+    }
+    println!("count: {}", count);
+    count
 }
 
+// convert input to [[0,1,0...]....] where 1 is if input char is #
+// in hindsight didn't need to waste time converting chars to 1 or 0 oh well
+fn parse_input(input: Vec<&str>) -> Vec<Vec<i8>> {
+    let mut grid = Vec::new();
+    for line in input {
+        let r = line
+            .chars()
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|c| if c == &'#' { 1 } else { 0 })
+            .collect::<Vec<i8>>();
+        grid.push(r);
+    }
+    grid
+}
 fn part2(_: &str) -> () {
     ()
 }
@@ -30,11 +65,51 @@ mod tests {
     use crate::*;
 
     #[test]
+    fn day03_test_parse_input() {
+        let test_grid = vec![
+            "..##.......",
+            "#...#...#..",
+            ".#....#..#.",
+            "..#.#...#.#",
+            ".#...##..#.",
+            "..#.##.....",
+            ".#.#.#....#",
+            ".#........#",
+            "#.##...#...",
+            "#...##....#",
+            ".#..#...#.#",
+        ];
+        let output = vec![
+            vec![0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            vec![1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+            vec![0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+            vec![0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+            vec![0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0],
+            vec![0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0],
+            vec![0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+            vec![0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            vec![1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+            vec![1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+            vec![0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+        ];
+        assert_eq!(parse_input(test_grid), output);
+    }
+    #[test]
     fn day03_part1_samples() {
-        let tests = vec![("sample1", ()), ("sample2", ()), ("sample3", ())];
-        for t in tests {
-            assert_eq!(part1(t.0), t.1);
-        }
+        let test_grid = vec![
+            "..##.......",
+            "#...#...#..",
+            ".#....#..#.",
+            "..#.#...#.#",
+            ".#...##..#.",
+            "..#.##.....",
+            ".#.#.#....#",
+            ".#........#",
+            "#.##...#...",
+            "#...##....#",
+            ".#..#...#.#",
+        ];
+        assert_eq!(part1(test_grid), 7);
     }
 
     #[test]
