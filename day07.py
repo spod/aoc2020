0b1f5b75da6ba754190d6d0d30b13945680539dd
Michael Collins <michael.collins@gmail.com>
2020-12-06 23:46:06 -0800

    Working day 7 part 1

--- day07.py
+++ day07.py
@@ -37,52 +37,41 @@ def test_parse_rule():
     for line in sample:
         print(line, parse_rule(line))
 
-def gold_bags(bag_to_bags):
-    TARGET = "shiny gold bag"
-    result = set()
-    remainder = set(bag_to_bags.keys())
-    for bag, contains in bag_to_bags.items():
-        if TARGET in contains:
-            result.add(bag)
-            remainder.discard(bag)
-    for bag in remainder:
-        if result & bag_to_bags[bag]:
-            result.add(bag)
-
-    
-    print("gold_bags::result: {}".format(result))
-    print("gold_bags::remainder: {}".format(remainder))
-    
-    return result
-
-
-def test_gold_bags():
-    input = {'light red bag': {'muted yellow bag', 'bright white bag'}, 'dark orange bag': {'muted yellow bag', 'bright white bag'}, 'bright white bag': {'shiny gold bag'}, 'muted yellow bag': {'shiny gold bag', 'faded blue bag'}, 'shiny gold bag': {'vibrant plum bag', 'dark olive bag'}, 'dark olive bag': {'dotted black bag', 'faded blue bag'}, 'vibrant plum bag': {'dotted black bag', 'faded blue bag'}}
-    result = {"bright white bag", "muted yellow bag", "dark orange bag", "light red bag"}
-    assert gold_bags(input) == result, "Invalid result!"
-
+def walk(visited, graph, target):
+    if target not in visited:  # no point in duplicating walks
+        visited.add(target)
+        for source in graph:
+            for child in graph[source]:
+                #print("source: {}, child: {}, graph[{}]: {}".format(source, child, source, graph[source]))
+                if target == child:
+                    # we can also find a path to target using
+                    # this bag as source so walk the graph using this as target bag
+                    walk(visited, graph, source)
 
 def part1(input):
     TARGET = "shiny gold bag"
-    count = 0
-    bag_to_bags = defaultdict(set)
 
     rules = [parse_rule(rule) for rule in input]
 
+    # build a graph of bags from rule set
+    bag_graph = defaultdict(set)
     for r in rules:
         for c in r.contains:
-            bag_to_bags[r.bag].add(c.bag)
+            bag_graph[r.bag].add(c.bag)
+        if len(r.contains) == 0:
+            bag_graph[r.bag] = set()
 
-    print("bag_to_bags: {}".format(bag_to_bags))
-    golden_bags = gold_bags(bag_to_bags)
-    print("golden_bags: {}".format(golden_bags))
+    # print("bag_graph: {}".format(bag_graph))
 
-    for r in rules:
-        if r.bag in golden_bags:
-            count += 1
+    # walk bag graph to find all paths to target
+    # track paths taken in visited
+    visited = set()
+    walk(visited, bag_graph, TARGET)
 
-    print("part1 count: {}".format(count))
+    # print("part1 visited: {}".format(visited))
 
+    count = len(visited) - 1 # exclude direct path to target
+    print("part1 count: {}", count)
     return count
 
 def test_part1_sample():
@@ -91,10 +80,9 @@ def test_part1_sample():
 
 def test_part1_real():
     input = list( (l.strip() for l in open('./inputs/day07').readlines()) )
-    assert part1(input) == 4, "Invalid result!"
+    assert part1(input) == 205, "Invalid result!"
 
 #test_parse_rule()
-test_part1_sample()
-#test_part1_real()
-#test_gold_bags()
+#test_part1_sample()
+test_part1_real()
 print("Ok!")
